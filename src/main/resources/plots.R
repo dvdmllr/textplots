b6dd78a001048633c51dbc7b75611665aed944b5
David Mueller <dvdmllr@outlook.com>
2015-01-03 23:21:03 +0100

    updating R code

--- src/main/resources/plots.R
+++ src/main/resources/plots.R
@@ -3,8 +3,12 @@ library(reshape2)
 # boxplot
 df <- data.frame(iris$Sepal.Width, iris$Sepal.Length) 
 md <- melt(df, variables=(c("iris.Sepal.Width", "iris.Sepal.Length")))
-qplot(factor(0),value,data=md, geom='boxplot', color=variable) + xlab("") + coord_flip() 
+qplot(factor(0),value,data=md, geom='boxplot', color=variable) + 
+  xlab("") + 
+  coord_flip() 
 # histogram
-ggplot(iris, aes(x=Sepal.Length)) + geom_histogram(binwidth=.45, colour="black", fill="white") + scale_x_reverse() + coord_flip() 
+ggplot(iris, aes(x=Sepal.Length)) +
+  geom_histogram(colour="black", fill="white", breaks=seq(4.3,7.9,0.45),right=TRUE) 
 # scatterplot
-ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + geom_point(shape=3)  
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+  geom_point(shape=3)
